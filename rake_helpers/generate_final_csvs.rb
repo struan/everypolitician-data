6e5d25a3f148456ef41330aac08b333922526a5f
Chris Mytton <chrismytton@gmail.com>
2016-01-11 14:13:04 +0000

    Extract logic for normalizing Twitter handles
    This has been extracted into an external gem that now gets called from
    csv_to_popolo.

--- rake_helpers/generate_final_csvs.rb
+++ rake_helpers/generate_final_csvs.rb
@@ -1,3 +1,4 @@
+require 'everypolitician/popolo'
 
 desc "Build the term-table CSVs"
 task :csvs => ['term_csvs:term_tables', 'term_csvs:name_list']
@@ -7,39 +8,6 @@ CLEAN.include('term-*.csv', 'names.csv')
 namespace :term_csvs do
 
   require 'csv'
-
-  def persons_twitter(p)
-    if p.key? :contact_details
-      if cd_twitter = p[:contact_details].find { |d| d[:type] == 'twitter' }
-        return cd_twitter[:value].strip
-      end
-    end
-
-    if p.key? 'links'
-      if l_twitter = p[:links].find { |d| d[:note][/twitter/i] }
-        return l_twitter[:url].strip
-      end
-    end
-  end
-
-  # https://twitter.com/tom_watson?lang=en
-  # https://twitter.com/search?q=%23EvelynMEP&src=typd
-
-  def standardised_twitter(t)
-    return if t.to_s.empty?
-    return $1 if t.match /^\@(\w+)$/
-    return $1 if t.match /^(\w+)$/
-    return $1 if t.match %r{(?:www.)?twitter.com/@?(\w+)$}i
-
-    # Odd special cases
-    return $1 if t.match %r{twitter.com/search\?q=%23(\w+)}i
-    return $1 if t.match %r{twitter.com/#!/https://twitter.com/(\w+)}i
-    return $1 if t.match %r{(?:www.)?twitter.com/#!/(\w+)[/\?]?}i
-    return $1 if t.match %r{(?:www.)?twitter.com/@?(\w+)[/\/]?}i
-    warn "Unknown twitter handle: #{t.to_s.magenta}"
-    return 
-  end
-
   def persons_facebook(p)
     (p[:links] || {}).find(->{{url: nil}}) { |d| d[:note] == 'facebook' }[:url]
   end
@@ -59,10 +27,11 @@ namespace :term_csvs do
 
   task :term_tables => 'ep-popolo-v1.0.json' do
     @json = JSON.parse(File.read('ep-popolo-v1.0.json'), symbolize_names: true )
+    popolo = EveryPolitician::Popolo::JSON.new(@json)
     terms = {}
 
     data = @json[:memberships].find_all { |m| m.key? :legislative_period_id }.map do |m|
-      person = @json[:persons].find       { |r| (r[:id] == m[:person_id])       || (r[:id].end_with? "/#{m[:person_id]}") }
+      person = popolo.persons.find        { |r| (r[:id] == m[:person_id])       || (r[:id].end_with? "/#{m[:person_id]}") }
       group  = @json[:organizations].find { |o| (o[:id] == m[:on_behalf_of_id]) || (o[:id].end_with? "/#{m[:on_behalf_of_id]}") }
       house  = @json[:organizations].find { |o| (o[:id] == m[:organization_id]) || (o[:id].end_with? "/#{m[:organization_id]}") }
       terms[m[:legislative_period_id]] ||= @json[:events].find { |e| e[:id].split('/').last == m[:legislative_period_id].split('/').last }
@@ -78,7 +47,7 @@ namespace :term_csvs do
         name: name_at(person, m[:end_date] || terms[m[:legislative_period_id]][:end_date]),
         sort_name: person[:sort_name].to_s.empty? ? person[:name] : person[:sort_name],
         email: person[:email],
-        twitter: standardised_twitter(persons_twitter(person)),
+        twitter: person.twitter,
         facebook: persons_facebook(person),
         group: group[:name],
         group_id: group[:id].split('/').last,
