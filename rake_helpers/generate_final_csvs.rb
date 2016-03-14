596b101c88f99ea42bcf69a0760a09f68a68ab79
Tony Bowden <tony@tmtm.com>
2016-03-14 19:18:56 +0000

    sort the names file by the downcased names
    Abdyldaev and ABDYLDAEV should be side by side, not separated by all
    the other ALLCAPS versions.

--- rake_helpers/generate_final_csvs.rb
+++ rake_helpers/generate_final_csvs.rb
@@ -74,7 +74,7 @@ namespace :term_csvs do
       nameset = Set.new([p[:name]])
       nameset.merge (p[:other_names] || []).map { |n| n[:name] }
       nameset.map { |n| [n, p[:id].split('/').last] }
-    }.flatten(1).uniq { |name, id| [name.downcase, id] }.sort_by { |name, id| [name, id] }
+    }.flatten(1).uniq { |name, id| [name.downcase, id] }.sort_by { |name, id| [name.downcase, id] }
 
     filename = "names.csv"
     header = %w(name id).to_csv
