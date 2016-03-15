1dc2035d6dfbd1feb03ef56a5e996399fee23036
Tony Bowden <tony@tmtm.com>
2016-03-15 13:01:29 +0000

    Handle the new Facebook link format from csv_to_popolo
    We don't need to run our own standardisation any more — that's now
    handled by csv_to_popolo. We just need to strip it back for the CSV
    format.

--- rake_helpers/generate_final_csvs.rb
+++ rake_helpers/generate_final_csvs.rb
@@ -10,15 +10,10 @@ CLEAN.include('term-*.csv', 'names.csv')
 namespace :term_csvs do
 
   def tidy_facebook_link(page)
+    # CSV-to-Popolo runs these through FacebookUsernameExtractor, so
+    # we can just strip off the prefix
     return if page.to_s.empty?
-    page = page.split(/(?=https?:)/).first 
-    return page[/profile.php\?id=(\w+)/, 1] if page.include? 'profile.php'
-    page = page.sub(/^https?:\/\//,'').sub(/^([a-z\-]+\.)?facebook.com/,'').sub(/^\/+/,'').sub(/\?.*/,'').sub(/^#!\//,'')
-    return if page.start_with? 'search/'
-    return page.sub('pages/','').sub('/','-') if page.start_with? 'pages/'
-    return page.split('/')[1] if page.start_with? 'people/'
-    return if page.include? 'facebook' # TODO handle more cases
-    return page
+    page.sub('https://facebook.com/','')
   end
 
   require 'csv'
