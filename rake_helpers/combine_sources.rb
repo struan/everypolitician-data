a0aab17bc0940ea07d381f6dd996f618aebd1d99
Tony Bowden <tony@tmtm.com>
2016-03-27 19:08:31 +0100

    No longer need the REMAP hash here
    We no longer load any CSV files ourselves, so can delete this

--- rake_helpers/combine_sources.rb
+++ rake_helpers/combine_sources.rb
@@ -41,38 +41,6 @@ namespace :merge_sources do
     end
   end
 
-  # TODO: this is being moved to Source
-  REMAP = {
-    area: %w(constituency region district place),
-    area_id: %w(constituency_id region_id district_id place_id),
-    biography: %w(bio blurb),
-    birth_date: %w(dob date_of_birth),
-    blog: %w(weblog),
-    cell: %w(mob mobile cellphone),
-    chamber: %w(house),
-    death_date: %w(dod date_of_death),
-    end_date: %w(end ended until to),
-    executive: %w(post),
-    family_name: %w(last_name surname lastname),
-    fax: %w(facsimile),
-    gender: %w(sex),
-    given_name: %w(first_name forename),
-    group: %w(party party_name faction faktion bloc block org organization organisation),
-    group_id: %w( party_id faction_id faktion_id bloc_id block_id org_id organization_id organisation_id),
-    image: %w(img picture photo photograph portrait),
-    name: %w(name_en),
-    patronymic_name: %w(patronym patronymic),
-    phone: %w(tel telephone),
-    source: %w(src),
-    start_date: %w(start started from since),
-    term: %w(legislative_period),
-    website: %w(homepage href url site),
-  }.each_with_object({}) { |(k, vs), mapped| vs.each { |v| mapped[v] = k } }
-
-  def remap(str)
-    REMAP[str.to_s] || str.to_sym
-  end
-
   @warnings = Set.new
   def warn_once(str)
     @warnings << str
