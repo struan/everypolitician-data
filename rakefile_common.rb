349e4a8e3861a7a90339a061586d43f139b59e06
Tony Bowden <tony@tmtm.com>
2016-05-10 07:38:33 +0100

    rake task for generating the Elections instructions
    call with (e.g.):
    
      ELECTION_BASE=Q22269333 be rake add_election_instructions

--- rakefile_common.rb
+++ rakefile_common.rb
@@ -163,6 +163,24 @@ task :build_p39s do
   File.write(@INSTRUCTIONS_FILE, JSON.pretty_generate(instr))
 end
 
+desc "Add Wikidata elections instructions"
+task :add_election_instructions do
+  instructions = clean_instructions_file
+  sources = instructions[:sources]
+  abort "Already have position instructions" if sources.find { |s| s[:type] == 'wikidata-elections' }
+  abort "No base: set ELECTION_BASE=Q…" unless ENV.key? 'ELECTION_BASE'
+
+  sources << { 
+    file: "wikidata/elections.json",
+    type: "wikidata-elections",
+    create: {
+      from: "election-wikidata",
+      base: ENV['ELECTION_BASE'],
+    },
+  } 
+  File.write(@INSTRUCTIONS_FILE, JSON.pretty_generate(instructions))
+end
+
 def instructions(key)
   @instructions ||= load_instructions_file
   @instructions[key]
