b17c97814c37207dc5bcaeeb99c156a82e72eb58
Tony Bowden <tony@tmtm.com>
2016-06-21 14:02:41 +0100

    Split out generator for P39s files

--- rakefile_common.rb
+++ rakefile_common.rb
@@ -154,26 +154,6 @@ task :build_parties do
   File.write('sources/manual/group_wikidata.csv', "id,wikidata\n" + data)
 end
 
-desc "Add a wikidata P39 file"
-task :build_p39s do
-  instr = clean_instructions_file
-  sources = instr[:sources]
-  abort "Already have position instructions" if sources.find { |s| s[:type] == 'wikidata-positions' }
-
-  wikidata = sources.find { |s| s[:type] == 'wikidata' } or abort "No wikidata section"
-  reconciliation = [wikidata[:merge]].flatten(1).find { |s| s.key? :reconciliation_file } or abort "No wikidata reconciliation file"
-
-  sources << { 
-    file: "wikidata/positions.json",
-    type: "wikidata-positions",
-    create: {
-      from: "wikidata-raw",
-      source: reconciliation[:reconciliation_file],
-    },
-  } 
-  write_instructions(instr)
-end
-
 def instructions(key)
   @instructions ||= load_instructions_file
   @instructions[key]
@@ -191,4 +171,5 @@ require_relative 'rake_build/generate_final_csvs.rb'
 require_relative 'rake_build/generate_stats.rb'
 
 require_relative 'rake_generate/election_info.rb'
+require_relative 'rake_generate/position_info.rb'
 
