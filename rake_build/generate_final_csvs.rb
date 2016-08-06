a61c4727246e83fe675f98402648d0fb4bc88248
Tony Bowden <tony@tmtm.com>
2016-08-06 18:00:15 +0100

    Rubocop fixups for generate_final_csvs

--- rake_build/generate_final_csvs.rb
+++ rake_build/generate_final_csvs.rb
@@ -2,30 +2,29 @@ require_relative '../lib/position_filterer'
 require 'everypolitician/popolo'
 require 'json5'
 
-desc "Build the term-table CSVs"
-task :csvs => ['term_csvs:term_tables', 'term_csvs:name_list', 'term_csvs:positions', 'term_csvs:reports']
+desc 'Build the term-table CSVs'
+task csvs: ['term_csvs:term_tables', 'term_csvs:name_list', 'term_csvs:positions', 'term_csvs:reports']
 
 CLEAN.include('term-*.csv', 'names.csv')
 
 namespace :term_csvs do
-
   def tidy_facebook_link(page)
     # CSV-to-Popolo runs these through FacebookUsernameExtractor, so
     # we can just strip off the prefix
     return if page.to_s.empty?
-    page.sub('https://facebook.com/','')
+    page.sub('https://facebook.com/', '')
   end
 
   require 'csv'
   desc 'Generate the Term Tables'
-  task :term_tables => 'ep-popolo-v1.0.json' do
-    @json = JSON.parse(File.read('ep-popolo-v1.0.json'), symbolize_names: true )
+  task term_tables: 'ep-popolo-v1.0.json' do
+    @json = JSON.parse(File.read('ep-popolo-v1.0.json'), symbolize_names: true)
     popolo = EveryPolitician::Popolo.read('ep-popolo-v1.0.json')
-    people = Hash[ popolo.persons.map { |p| [p.id, p] } ]
+    people = Hash[popolo.persons.map { |p| [p.id, p] }]
     term_end_dates = Hash[popolo.terms.map { |t| [t.id, t.end_date] }]
 
-    data = @json[:memberships].find_all { |m| m.key? :legislative_period_id }.map do |m|
-      person = people[ m[:person_id] ]
+    data = @json[:memberships].select { |m| m.key? :legislative_period_id }.map do |m|
+      person = people[m[:person_id]]
       group  = @json[:organizations].find { |o| (o[:id] == m[:on_behalf_of_id]) || (o[:id].end_with? "/#{m[:on_behalf_of_id]}") }
       house  = @json[:organizations].find { |o| (o[:id] == m[:organization_id]) || (o[:id].end_with? "/#{m[:organization_id]}") }
 
@@ -36,22 +35,22 @@ namespace :term_csvs do
       end
 
       {
-        id: person.id.split('/').last,
-        name: person.name_at(m[:end_date] || term_end_dates[m[:legislative_period_id]]),
-        sort_name: person.sort_name,
-        email: person.email,
-        twitter: person.twitter,
-        facebook: tidy_facebook_link(person.facebook),
-        group: group[:name],
-        group_id: group[:id].split('/').last,
-        area_id: m[:area_id],
-        area: m[:area_id] && @json[:areas].find { |a| a[:id] == m[:area_id] }[:name],
-        chamber: house[:name],
-        term: m[:legislative_period_id].split('/').last,
+        id:         person.id.split('/').last,
+        name:       person.name_at(m[:end_date] || term_end_dates[m[:legislative_period_id]]),
+        sort_name:  person.sort_name,
+        email:      person.email,
+        twitter:    person.twitter,
+        facebook:   tidy_facebook_link(person.facebook),
+        group:      group[:name],
+        group_id:   group[:id].split('/').last,
+        area_id:    m[:area_id],
+        area:       m[:area_id] && @json[:areas].find { |a| a[:id] == m[:area_id] }[:name],
+        chamber:    house[:name],
+        term:       m[:legislative_period_id].split('/').last,
         start_date: m[:start_date],
-        end_date: m[:end_date],
-        image: person.image,
-        gender: person.gender,
+        end_date:   m[:end_date],
+        image:      person.image,
+        gender:     person.gender,
       }
     end
 
@@ -60,17 +59,19 @@ namespace :term_csvs do
     terms.each do |t, rs|
       filename = "term-#{t}.csv"
       header = rs.first.keys.to_csv
-      rows   = rs.portable_sort_by { |r| [r[:name], r[:id], r[:start_date].to_s, r[:area].to_s ] }.map { |r| r.values.to_csv }
+      rows   = rs.portable_sort_by { |r| [r[:name], r[:id], r[:start_date].to_s, r[:area].to_s] }.map { |r| r.values.to_csv }
       csv    = [header, rows].compact.join
       File.write(filename, csv)
     end
   end
 
-  task :top_identifiers => :term_tables do
-    top_identifiers = @json[:persons].map { |p| (p[:identifiers] || []).map { |i| i[:scheme] } }.flatten.
-      reject { |i| i == 'everypolitician_legacy' }.group_by { |i| i }.
-      sort_by { |i, is| -is.count }.take(5).
-      map { |i, is| [i, is.count] }
+  task top_identifiers: :term_tables do
+    top_identifiers = @json[:persons].map { |p| (p[:identifiers] || []).map { |i| i[:scheme] } }.flatten
+                                     .reject { |i| i == 'everypolitician_legacy' }
+                                     .group_by { |i| i }
+                                     .sort_by { |_i, is| -is.count }
+                                     .take(5)
+                                     .map { |i, is| [i, is.count] }
 
     if top_identifiers.any?
       warn "\nTop identifiers:"
@@ -81,27 +82,28 @@ namespace :term_csvs do
     end
   end
 
-  task :name_list => :top_identifiers do
-    names = @json[:persons].map { |p|
+  task name_list: :top_identifiers do
+    names = @json[:persons].flat_map do |p|
       nameset = Set.new([p[:name]])
       nameset.merge (p[:other_names] || []).map { |n| n[:name] }
       nameset.map { |n| [n, p[:id].split('/').last] }
-    }.flatten(1).uniq { |name, id| [name.downcase, id] }.sort_by { |name, id| [name.downcase, id] }
+    end.uniq { |name, id| [name.downcase, id] }.sort_by { |name, id| [name.downcase, id] }
 
-    filename = "names.csv"
+    filename = 'names.csv'
     header = %w(name id).to_csv
     csv    = [header, names.map(&:to_csv)].compact.join
     warn "Creating #{filename}"
     File.write(filename, csv)
   end
 
-  desc "Add some final reporting information"
-  task :reports => :term_tables do
+  desc 'Add some final reporting information'
+  task reports: :term_tables do
     wikidata_persons = @json[:persons].partition { |p| (p[:identifiers] || []).find { |i| i[:scheme] == 'wikidata' } }
-    wikidata_parties = @json[:organizations].select { |o| o[:classification] == 'party' }.
-      reject { |p| p[:name].downcase == 'unknown' }.
-      partition { |p| (p[:identifiers] || []).find { |i| i[:scheme] == 'wikidata' } 
-    }
+    wikidata_parties = @json[:organizations].select { |o| o[:classification] == 'party' }
+                                            .reject { |p| p[:name].downcase == 'unknown' }
+                                            .partition do |p|
+      (p[:identifiers] || []).find { |i| i[:scheme] == 'wikidata' }
+    end
     matched, unmatched = wikidata_persons.map(&:count)
     warn "Persons matched to Wikidata: #{matched} ✓ #{unmatched.zero? ? '' : "| #{unmatched} ✘"}"
     wikidata_persons.last.shuffle.take(10).each { |p| warn "  No wikidata: #{p[:name]} (#{p[:id]})" } unless matched.zero?
@@ -112,68 +114,68 @@ namespace :term_csvs do
   end
 
   desc 'Build the Positions file'
-  task :positions => ['ep-popolo-v1.0.json'] do
+  task positions: ['ep-popolo-v1.0.json'] do
     next unless POSITION_RAW.file?
     warn "Creating #{POSITION_CSV}"
-    positions = JSON.parse(POSITION_RAW.read, symbolize_names: true) 
+    positions = JSON.parse(POSITION_RAW.read, symbolize_names: true)
     filter    = if POSITION_FILTER.exist?
-      # read with JSON5 to be more liberal about trailing commas. 
-      # But it doesn't have a 'symbolize_names' so rountrip through JSON
-      JSON.parse(JSON5.parse(POSITION_FILTER.read).to_json, symbolize_names: true).each do |s, fs|
-        fs.each { |_,fs| fs.each { |f| f.delete :count } }
-      end
-    else 
-      { exclude: { self: [], other: [] }, include: { self: [], other_legislatures: [], executive: [], party: [], other: [] } }
-    end
+                  # read with JSON5 to be more liberal about trailing commas.
+                  # But it doesn't have a 'symbolize_names' so rountrip through JSON
+                  JSON.parse(JSON5.parse(POSITION_FILTER.read).to_json, symbolize_names: true).each do |_s, fs|
+                    fs.each { |_, fs| fs.each { |f| f.delete :count } }
+                  end
+                else
+                  { exclude: { self: [], other: [] }, include: { self: [], other_legislatures: [], executive: [], party: [], other: [] } }
+                end
 
     to_include = filter[:include].map { |_, fs| fs.map { |f| f[:id] } }.flatten.to_set
     to_exclude = filter[:exclude].map { |_, fs| fs.map { |f| f[:id] } }.flatten.to_set
     executive  = filter[:include][:executive].map { |p| p[:id] }.to_set
 
-    want, unknown = @json[:persons].map { |p| 
-      (p[:identifiers] || []).find_all { |i| i[:scheme] == 'wikidata' }.map { |id|
-        positions[id[:identifier].to_sym].to_a.reject { |p| p[:id].nil? }.map { |posn| 
+    want, unknown = @json[:persons].map do |p|
+      (p[:identifiers] || []).select { |i| i[:scheme] == 'wikidata' }.map do |id|
+        positions[id[:identifier].to_sym].to_a.reject { |p| p[:id].nil? }.map do |posn|
           {
-            id: p[:id],
-            wikidata: id[:identifier],
-            name: p[:name],
+            id:          p[:id],
+            wikidata:    id[:identifier],
+            name:        p[:name],
             position_id: posn[:id],
-            position: posn[:label],
+            position:    posn[:label],
             description: posn[:description],
-            start_date: (posn[:qualifiers] || {})[:P580],
-            end_date: (posn[:qualifiers] || {})[:P582],
+            start_date:  (posn[:qualifiers] || {})[:P580],
+            end_date:    (posn[:qualifiers] || {})[:P582],
           }
-        }
-      }
-    }.flatten(2).reject { |r| to_exclude.include? r[:position_id] }.partition { |r| to_include.include? r[:position_id] }
+        end
+      end
+    end.flatten(2).reject { |r| to_exclude.include? r[:position_id] }.partition { |r| to_include.include? r[:position_id] }
 
     want.select { |p| executive.include? p[:position_id] }.select { |p| p[:start_date].nil? && p[:end_date].nil? }.each do |p|
       warn "  ☇ No dates for #{p[:name]} (#{p[:wikidata]}) as #{p[:position]}"
     end
 
-    (filter[:unknown] ||= {})[:unknown] = unknown.
-      group_by { |u| u[:position_id] }.
-      sort_by { |u, us| us.first[:position].downcase }.
-      map { |id, us| { id: id, name: us.first[:position], description: us.first[:description], count: us.count, example: us.first[:wikidata] } }.each do |u|
-        warn "  Unknown position (x#{u[:count]}): #{u[:id]} #{u[:name]} — e.g. #{u.delete :example}"
-      end
+    (filter[:unknown] ||= {})[:unknown] = unknown
+                                          .group_by { |u| u[:position_id] }
+                                          .sort_by { |_u, us| us.first[:position].downcase }
+                                          .map { |id, us| { id: id, name: us.first[:position], description: us.first[:description], count: us.count, example: us.first[:wikidata] } }.each do |u|
+      warn "  Unknown position (x#{u[:count]}): #{u[:id]} #{u[:name]} — e.g. #{u.delete :example}"
+    end
 
     filter.each do |_, section|
-      section.each { |k, vs| vs.sort_by! { |e| e[:name] } }
+      section.each { |_k, vs| vs.sort_by! { |e| e[:name] } }
     end
     csv_columns = %w(id name position start_date end_date)
-    csv    = [csv_columns.to_csv, want.map { |p| csv_columns.map { |c| p[c.to_sym] }.to_csv }].compact.join
+    csv = [csv_columns.to_csv, want.map { |p| csv_columns.map { |c| p[c.to_sym] }.to_csv }].compact.join
 
     POSITION_CSV.dirname.mkpath
-    File.write(POSITION_CSV, csv) # POSITION_CSV.write(csv) needs ruby 2.1.0
+    File.write(POSITION_CSV, csv) # POSITION_CSV.write(csv) needs ruby 2.1.0
     File.write(POSITION_FILTER, JSON.pretty_generate(filter))
 
-    if filter[:unknown][:unknown].any? && ENV['GENERATE_POSITION_INTERFACE'] 
+    if filter[:unknown][:unknown].any? && ENV['GENERATE_POSITION_INTERFACE']
       html = Position::Filterer.new(filter).html
       File.write('sources/manual/.position-filter.html', html)
       FileUtils.copy('../../../templates/position-filter.js', 'sources/manual/.position-filter.js')
-      warn "open sources/manual/.position-filter.html".yellow
-      warn "pbpaste | bundle exec ruby ../../../bin/learn_position.rb sources/manual/position-filter.json".yellow
+      warn 'open sources/manual/.position-filter.html'.yellow
+      warn 'pbpaste | bundle exec ruby ../../../bin/learn_position.rb sources/manual/position-filter.json'.yellow
     end
   end
 end
