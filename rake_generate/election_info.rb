84228c7b500e21dd6c7841ef7c32532e80571874
Tony Bowden <tony@tmtm.com>
2016-08-06 17:18:50 +0100

    Rubocop autofixes for rake_generate

--- rake_generate/election_info.rb
+++ rake_generate/election_info.rb
@@ -1,19 +1,18 @@
 
-desc "Add Wikidata elections instructions"
+desc 'Add Wikidata elections instructions'
 task :add_election_instructions do
   instructions = clean_instructions_file
   sources = instructions[:sources]
-  abort "Already have position instructions" if sources.find { |s| s[:type] == 'wikidata-elections' }
-  abort "No base: set ELECTION_BASE=Q…" unless ENV.key? 'ELECTION_BASE'
+  abort 'Already have position instructions' if sources.find { |s| s[:type] == 'wikidata-elections' }
+  abort 'No base: set ELECTION_BASE=Q…' unless ENV.key? 'ELECTION_BASE'
 
-  sources << { 
-    file: "wikidata/elections.json",
-    type: "wikidata-elections",
+  sources << {
+    file:   'wikidata/elections.json',
+    type:   'wikidata-elections',
     create: {
-      from: "election-wikidata",
+      from: 'election-wikidata',
       base: ENV['ELECTION_BASE'],
     },
-  } 
+  }
   File.write(@INSTRUCTIONS_FILE, JSON.pretty_generate(instructions))
 end
-
--- rake_generate/position_info.rb
+++ rake_generate/position_info.rb
@@ -1,20 +1,20 @@
 
-desc "Add a wikidata P39 file"
+desc 'Add a wikidata P39 file'
 task :build_p39s do
   instr = clean_instructions_file
   sources = instr[:sources]
-  abort "Already have position instructions" if sources.find { |s| s[:type] == 'wikidata-positions' }
+  abort 'Already have position instructions' if sources.find { |s| s[:type] == 'wikidata-positions' }
 
-  wikidata = sources.find { |s| s[:type] == 'wikidata' } or abort "No wikidata section"
-  reconciliation = [wikidata[:merge]].flatten(1).find { |s| s.key? :reconciliation_file } or abort "No wikidata reconciliation file"
+  (wikidata = sources.find { |s| s[:type] == 'wikidata' }) || abort('No wikidata section')
+  (reconciliation = [wikidata[:merge]].flatten(1).find { |s| s.key? :reconciliation_file }) || abort('No wikidata reconciliation file')
 
-  sources << { 
-    file: "wikidata/positions.json",
-    type: "wikidata-positions",
+  sources << {
+    file:   'wikidata/positions.json',
+    type:   'wikidata-positions',
     create: {
-      from: "wikidata-raw",
+      from:   'wikidata-raw',
       source: reconciliation[:reconciliation_file],
     },
-  } 
+  }
   write_instructions(instr)
 end
